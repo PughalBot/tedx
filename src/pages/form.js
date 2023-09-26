62f8c7612c400587a227f161b536c4c971c74dd7
PughalBot <77720496+PughalBot@users.noreply.github.com>
2023-09-26 14:17:52 +0530

    form edit

--- src/pages/form.js
+++ src/pages/form.js
@@ -5,19 +5,20 @@ function FormPage() {
   return (
     <>
     <div className="w-screen h-screen bg-[#2c1262] flex justify-center items-center">
-    <img src="/x.png" alt="Logo" className=" opacity-75 h-full absolute w-auto" />
-    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="p-4 bg-[#2c1262] w-11/12 md:w-96 mx-auto bg-white shadow-lg rounded z-10 required">
+    <img src="/x.png" alt="Logo" className=" opacity-75 md:h-full absolute w-auto" />
+    <div className="p-4 bg-white w-11/12 bg-opacity-80 md:w-96 mx-auto shadow-3xl rounded-lg z-10 required">
       <form action="https://docs.google.com/forms/d/e/1FAIpQLScTAT3t-z4a_spCLHZjQnjBf1_4wYdCK-XYKcwF7xmCfKfG9A/formResponse" >
         <div className="flex justify-center">
           <img src="/x.png" alt="Logo" className="h-16  w-auto" /> 
         </div> 
         <div className="mb-4">
-          <label className="block mb-2">Name</label>
-          <input type="text" name="entry.1512446941" className="w-full p-2 border rounded required" />
+          <label className="block text-[#db1068] font-js font-bold mb-2">Name</label>
+          <input type="text" name="entry.1512446941" className="w-full p-2 rounded required" placeholder="Name"/>
         </div>
         <div className="mb-4">
-          <label className="block mb-2">Year</label>
-          <select name="entry.586898713" className="w-full p-2 border rounded required">
+          <label className="block text-[#db1068] font-js font-bold mb-2">Year</label>
+          <select name="entry.586898713" className="w-full p-2 border-[#db1068] rounded required">
+            <option value="">Select Year</option>
             <option value="I">I</option>
             <option value="II">II</option>
             <option value="III">III</option>
@@ -26,8 +27,9 @@ function FormPage() {
           </select>
         </div>
         <div className="mb-4">
-          <label className="block mb-2">Department</label>
-          <select name="entry.374063172" className="w-full p-2 border rounded required">
+          <label className="block text-[#db1068] font-js font-bold mb-2">Department</label>
+          <select name="entry.374063172" className="w-full p-2 border-[#db1068] rounded required">
+            <option value="">Select Department</option>
             <option value="AIDS">AIDS</option>
             <option value="CSBS">CSBS</option>
             <option value="EEE">EEE</option>
@@ -47,8 +49,9 @@ function FormPage() {
           </select>
         </div>
         <div className="mb-4">
-          <label className="block mb-2">Section</label>
-          <select name="entry.1292046826" className="w-full p-2 border rounded required">
+          <label className="block text-[#db1068] font-js font-bold mb-2">Section</label>
+          <select name="entry.1292046826" className="w-full p-2 border-[#db1068] rounded required">
+            <option value="">Select Section</option>
             <option value="A">A</option>
             <option value="B">B</option>
             <option value="C">C</option>
@@ -56,20 +59,20 @@ function FormPage() {
           </select>
         </div>
         <div className="mb-4">
-          <label className="block mb-2">Email</label>
-          <input type="email" name="entry.663978457" className="w-full p-2 border rounded required" />
+          <label className="block text-[#db1068] font-js font-bold mb-2">Email</label>
+          <input type="email" name="entry.663978457" className="w-full p-2 border-[#db1068] rounded required" placeholder="example@example.com" />
         </div>
         <div className="mb-4">
-          <label className="block mb-2">Phone</label>
-          <input type="text" name="entry.116614212" className="w-full p-2 border rounded required" />
+          <label className="block text-[#db1068] font-js font-bold mb-2">Phone</label>
+          <input type="text" name="entry.116614212" className="w-full p-2 border-[#db1068] rounded required" placeholder="+919840457238"/>
         </div>
         <div className="flex justify-center">
-        <button type="submit" className="mt-4 bg-[#db1068] text-white font-bold py-2 px-4 rounded required">
+        <button type="submit" className="mt-4 bg-[#db1068] font-js text-white font-bold py-2 px-4 rounded required">
           Submit
         </button>
         </div>
       </form>
-    </motion.div>
+    </div>
     </div>
     </>
   );
