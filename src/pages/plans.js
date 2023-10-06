2195dd05db295923335c5b170070105c7126037c
PughalBot <pughal2003@gmail.com>
2023-10-06 23:49:05 +0530

    plans pages updated

--- src/pages/plans.js
+++ src/pages/plans.js
@@ -14,30 +14,35 @@ export default function Home() {
     </Head>
 
     <div className="min-h-screen flex items-center justify-center bg-[#2c1262]">
-      <div className="p-8 space-y-8 bg-white shadow-lg rounded-3xl">
+      <div className="p-8 space-y-8 bg-white shadow-lg rounded-lg">
         <h1 className="text-4xl md:text-5xl text-center text-[#db1068] font-hj">Choose Your Pass</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           
-          <Link href="https://rzp.io/l/GeteralPass">
-            <motion.div
-                whileHover={{ scale: 1.05 }}
-                className="p-6 bg-gray-200 md:h-96 rounded-3xl text-center"
-            >
-                <h2 className="text-xl font-bold mb-4">General Pass</h2>
-                <p className='text-sm md:text-md font-semibold '>◆ Goodies</p>
-                <p className='text-sm md:text-md font-semibold mt-4'>◆ Goodies</p>
-            </motion.div>
-          </Link>
-
-          <Link href="https://rzp.io/l/TedxGoldPass">
+        <Link href="https://rzp.io/l/TedxGoldPass">
           <motion.div
             whileHover={{ scale: 1.05 }}
-            className="p-6 bg-yellow-400 bg-cover md:h-96 rounded-3xl text-center"
+            className="p-6 bg-yellow-400 md:w-80 bg-cover md:h-hit rounded-3xl"
           >
-            <h2 className="text-xl font-bold mb-4">Gold Pass</h2>
-            <p className='text-sm md:text-md font-semibold '>◆ Interactive Session with Speakers</p>
-            <p className='text-sm md:text-md font-semibold mt-4'>◆ Goodies</p>
+            <h2 className="text-xl font-bold text-center mb-4">Premium Pass</h2>
+            <p className='text-sm md:text-md'>Elevate your TEDx experience with the Premium Pass. Immerse yourself in exclusive sessions and cherish added memorabilia that commemorates your time with us.</p>
+            <p className='text-sm md:text-md mt-4'><span className='font-semibold'>◆ Interactive Session with Speakers: </span>Engage in intimate conversations and deepen your understanding over a luxurious high tea with our esteemed speakers.</p>
+            <p className='text-sm md:text-md mt-4'><span className='font-semibold'>◆ Exclusive Tees: </span>Elevate your style with our exclusive TEDx-themed t-shirts</p>
+            <p className='text-sm md:text-md mt-4 mb-6'><span className='font-semibold'>◆ Relish all the advantages that come with the Standard Pass: </span>Elevate your TEDxSriSairamEngineeringCollege experience with this premium offering</p>
           </motion.div>
+          </Link>
+
+
+          <Link href="https://rzp.io/l/GeneralPass">
+            <motion.div
+                whileHover={{ scale: 1.05 }}
+                className="p-6 bg-gray-200 md:w-80 md:h-fit rounded-3xl"
+            >
+                <h2 className="text-xl font-bold text-center mb-4">Standard Pass</h2>
+                <p className='text-sm md:text-md'>Experience TEDx at its core with our Standard Pass. Dive into a day of inspiration and enlightenment, and take home memories curated just for you. Your Standard Pass includes:</p>
+                <p className='text-sm md:text-md mt-4'><span className='font-semibold'>◆ Gift Bag: </span>Indulge in an exclusive Gift Bag thoughtfully curated by TEDxSriSairamEngineeringCollege</p>
+                <p className='text-sm md:text-md mt-4'><span className='font-semibold'>◆ Certificate: </span>Commemorate your participation with an official Attendee Certificate, a token of your TEDxSriSairamEngineeringCollege experience.</p>
+                <p className='text-sm md:text-md mt-4 mb-1'><span className='font-semibold'>◆ Lunch: </span>Savor a delectable lunch spread prepared to tantalize your taste buds, ensuring a delightful culinary journey during your TEDx event.</p>
+            </motion.div>
           </Link>
 
         </div>
